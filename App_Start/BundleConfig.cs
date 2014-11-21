e6480117d5ad1c8147989fb79a4b0203174eeb56
belugabob <g1tph1l3!>
2014-11-21 21:00:57 +0000

    Started angular integration
    But couldn't get map to display correctly within a tab, so am checking
    in, prior to moving the map stuff into an angular controller

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace belugabrain.bikeitorhikeit
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-ui/ui-bootstrap-tpls.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
