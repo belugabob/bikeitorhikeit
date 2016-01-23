304cfb52483b4291e78a4acf327e37874b16304a
belugabob <g1tph1l3!>
2016-01-23 17:57:34 +0000

    Latest changes

---
+++ Areas/CTN/CTNAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace belugabrain.bikeitorhikeit.Areas.CTN
+{
+    public class CTNAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CTN";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CTN_default",
+                "CTN/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new [] {"belugabrain.bikeitorhikeit.Areas.CTN.Controllers"}
+            );
+        }
+    }
+}
---
+++ Areas/CTN/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace belugabrain.bikeitorhikeit.Areas.CTN.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: CTN/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
